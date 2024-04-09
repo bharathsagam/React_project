7bc778a9174890ffb47284ac729a17d83a2457f0
Bharath <sagambharath@gmail.com>
2024-04-09 21:19:39 +0530

    react_project

--- OneDrive/Desktop/Netflixreact/src/home/Home.jsx
+++ OneDrive/Desktop/Netflixreact/src/home/Home.jsx
@@ -1,4 +1,5 @@
  
+import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
 import  "./home.scss";
  
@@ -7,56 +8,7 @@ const Home = () => {
     <div className='home'>
 
         <Navbar/>
-        <img 
-         width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        v
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem ex, totam vitae quos fugiat dicta aut hic voluptates ea voluptatem labore eveniet possimus magni cum, delectus reiciendis sunt expedita ullam.
+        <Featured type="movie"/>
     </div>
   )
 }
